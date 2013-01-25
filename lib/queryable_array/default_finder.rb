e8ef6d668c0c204748956fe7827fc3dbc8587e39
Sean Huber <shuber@einsteinmedical.com>
2013-01-25 14:51:57 -0800

    Add more documentation for DefaultFinder [ci skip]

--- lib/queryable_array/default_finder.rb
+++ lib/queryable_array/default_finder.rb
@@ -11,19 +11,26 @@ class QueryableArray < Array
       self.default_finders = Array(default_finders)
     end
 
-    # If +default_finders+ has been set and +key+ is not a +Fixnum+ then it
-    # loops thru each +default_finders+ and returns the first matching result
-    # of +find_by(finder => key)+ or +find_all(finder => key.first)+ if +key+
-    # is an +Array+. If +key+ is already a +Hash+ or an +Array+ containing one
-    # then it acts like an alias for +find_by+ or +find_all+ respectively. It
-    # behaves exactly like its superclass +Array+ in all other cases.
+    # If +default_finders+ has been set and +key+ is not a +Fixnum+, +Range+,
+    # or anything else natively supported by +Array+ then it loops thru each
+    # +default_finders+ and returns the first matching result of +find_by(finder => key)+
+    # or +find_all(finder => key.first)+ if +key+ is an +Array+. If +key+ is already
+    # a +Hash+ or an +Array+ containing a +Hash+ then it acts like an alias for +find_by+
+    # or +find_all+ respectively. It also accepts a +Proc+ or any object that responds to
+    # +call+. It behaves exactly like its superclass +Array+ in all other cases.
     #
     #   pages = QueryableArray.new(Page.all, [:uri, :name])
     #
-    #   pages['/']        # => #<Page @uri='/'>
-    #   pages['Home']     # => #<Page @name='Home'>
-    #   pages[/home/i]    # => #<Page @name='Home'>
+    #   pages['/']        # => #<Page @uri='/' @name='Home'>
+    #   pages['Home']     # => #<Page @uri='/' @name='Home'>
+    #   pages[/home/i]    # => #<Page @uri='/' @name='Home'>
     #   pages['missing']  # => nil
+    #
+    #   pages[[/users/i]]    # => [#<Page @uri='/users/bob' @name='Bob'>, #<Page @uri='/users/steve' @name='Steve'>]
+    #   pages[[/missing/i]]  # => []
+    #
+    #   pages[proc { |page| page.uri == '/' }]         # => #<Page @uri='/' @name='Home'>
+    #   pages[[proc { |page| page.uri =~ /users/i }]]  # => [#<Page @uri='/users/bob' @name='Bob'>, #<Page @uri='/users/steve' @name='Steve'>]
     def [](key)
       super
     rescue TypeError => error
@@ -35,6 +42,10 @@ class QueryableArray < Array
       end
     end
 
+    # Converts a search into a +Proc+ object that can be passed to +find_by+ or
+    # +find_all+. If +search+ is a +Proc+ or an object that responds to +call+
+    # then it is wrapped in a +Proc+ and returned. Otherwise the returned +Proc+
+    # loops thru each +default_finders+ looking for a value that matches +search+.
     def query(search)
       Proc.new do |object|
         proc = search.respond_to?(:call) ? search : Proc.new do |object|
